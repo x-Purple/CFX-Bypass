fe4973b790063263d7b81ecba3e6c494ba8b81bc
SpeedyThePaster <zocggg@gmail.com>
2022-01-17 09:25:14 +0200

    Update network.cpp

--- smallcock/core/network.cpp
+++ smallcock/core/network.cpp
@@ -22,23 +22,23 @@ void network::unblock_connection(std::string process)
 void network::setup()
 {
 	// Locate FiveM.exe
-	auto fivem_path = g_trace->m_fivem_path + "\\" + "FiveM.exe";
+	auto fivem_path = g_trace->m_fivem_path + "\\FiveM.exe";
 	block_connection(fivem_path);
 
 
 	// Locate FiveM Subprocesses path
-	auto subprocess_path = g_trace->m_fivem_path + "\\" + "FiveM.app" + "\\" + "data" + "\\" + "cache" + "\\" "subprocess";
+	auto subprocess_path = g_trace->m_fivem_path + "\\FiveM.app\\data\\cache\\subprocess";
 
 	std::vector<std::string> gta_versions{ "b2545_", "b2372_", "b2189_", "b2060_",  "" };
 
 	for (auto processes : gta_versions)
 	{
 		// Block the gta processes
-		auto gta_process = subprocess_path + "\\" + "FiveM_" + processes + "GTAProcess.exe";
+		auto gta_process = subprocess_path + "\\FiveM_" + processes + "GTAProcess.exe";
 		block_connection(gta_process);
 
 		// Block the steam processes
-		auto steam_process = subprocess_path + "\\" + "FiveM_" + processes + "SteamChild.exe";
+		auto steam_process = subprocess_path + "\\FiveM_" + processes + "SteamChild.exe";
 		block_connection(steam_process);
 	}
 
@@ -52,18 +52,18 @@ void network::destroy()
 	unblock_connection(fivem_path);
 
 	// Locate FiveM Subprocesses path
-	auto subprocess_path = g_trace->m_fivem_path + "\\" + "FiveM.app" + "\\" + "data" + "\\" + "cache" + "\\" "subprocess";
+	auto subprocess_path = g_trace->m_fivem_path + "\\FiveM.app\\data\\cache\\subprocess";
 
 	std::vector<std::string> gta_versions{ "b2545_", "b2372_", "b2189_", "b2060_",  "" };
 
 	for (auto processes : gta_versions)
 	{
 		// Block the gta processes
-		auto gta_process = subprocess_path + "\\" + "FiveM_" + processes + "GTAProcess.exe";
+		auto gta_process = subprocess_path + "\\FiveM_" + processes + "GTAProcess.exe";
 		unblock_connection(gta_process);
 
 		// Block the steam processes
-		auto steam_process = subprocess_path + "\\" + "FiveM_" + processes + "SteamChild.exe";
+		auto steam_process = subprocess_path + "\\FiveM_" + processes + "SteamChild.exe";
 		unblock_connection(steam_process);
 	}
 }
